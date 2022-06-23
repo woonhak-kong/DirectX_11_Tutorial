70ba3fcea34df3d988e7f04add57e5de715ab9d0
woonhak-kong <gamdekong@gmail.com>
2022-06-23 15:21:31 -0400

    Tutorial_2

--- Tutorial/graphicsclass.h
+++ Tutorial/graphicsclass.h
@@ -9,16 +9,17 @@ const float SCREEN_DEPTH = 1000.0f;
 const float SCREEN_NEAR = 0.1f;
 
 
+class D3DClass;
 
 class GraphicsClass
 {
 public:
 	GraphicsClass();
-	GraphicsClass(const GraphicsClass&);
+	GraphicsClass(const GraphicsClass& other);
 	~GraphicsClass();
 
 
-	bool Initialize(int, int, HWND);
+	bool Initialize(int screenWidth, int screenHeight, HWND hwnd);
 	void Shutdown();
 	bool Frame();
 
@@ -26,7 +27,7 @@ private:
 	bool Render();
 
 private:
-
+	D3DClass* m_D3D;
 };
 
 #endif // _GRAPHICSCLASS_H_
