79da516301e50681aebbdf30005c30c8471b5ed1
woonhak-kong <gamdekong@gmail.com>
2022-07-06 16:38:03 -0400

    FontShaderClass is implemented

---
+++ Tutorial/FontShaderClass.h
@@ -0,0 +1,56 @@
+#pragma once
+#ifndef _FONTSHADERCLASS_H_
+#define _FONTSHADERCLASS_H_
+
+
+class FontShaderClass
+{
+
+private:
+	struct ConstantBufferType
+	{
+		XMMATRIX world;
+		XMMATRIX view;
+		XMMATRIX projection;
+	};
+
+	struct PixelBufferType
+	{
+		XMFLOAT4 pixelColor;
+	};
+
+
+public:
+	FontShaderClass();
+	FontShaderClass(const FontShaderClass& other);
+	~FontShaderClass();
+
+	bool Initialize(ID3D11Device* device, HWND hwnd);
+	void Shutdown();
+	bool Render(ID3D11DeviceContext* deviceContext, int indexCount, XMMATRIX worldMatrix, XMMATRIX viewMatrix, XMMATRIX projectionMatrix, ID3D11ShaderResourceView* texture, XMFLOAT4 pixelColor);
+
+
+private:
+	bool InitializeShader(ID3D11Device* device, HWND hwnd, const WCHAR* vsFilename, const WCHAR* psFilename);
+	void ShutdownShader();
+	void OutputShaderErrorMessage(ID3D10Blob* errorMessage, HWND hwnd, const WCHAR* shaderFilename);
+
+	bool SetShaderParameters(ID3D11DeviceContext* deviceContext, XMMATRIX worldMatrix, XMMATRIX viewMatrix, XMMATRIX projectionMatrix, ID3D11ShaderResourceView* texture, XMFLOAT4 pixelColor);
+	void RenderShader(ID3D11DeviceContext* deviceContext, int indexCount);
+
+private:
+	ID3D11VertexShader* m_vertexShader = nullptr;
+	ID3D11PixelShader* m_pixelShader = nullptr;
+	ID3D11InputLayout* m_layout = nullptr;
+	ID3D11Buffer* m_constantBuffer = nullptr;
+
+	ID3D11SamplerState* m_sampleState = nullptr;
+	ID3D11Buffer* m_pixelBuffer = nullptr;
+
+
+};
+
+
+
+
+#endif // _FONTSHADERCLASS_H_
