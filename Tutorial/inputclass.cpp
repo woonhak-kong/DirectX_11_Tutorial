1bffac9740528b5e214469e0711ac4138717bf8b
woonhak-kong <gamdekong@gmail.com>
2022-07-07 16:13:43 -0400

    InputClass is modified for DirectX Input system

--- Tutorial/inputclass.cpp
+++ Tutorial/inputclass.cpp
@@ -5,7 +5,7 @@ InputClass::InputClass()
 {
 }
 
-InputClass::InputClass(const InputClass&)
+InputClass::InputClass(const InputClass& other)
 {
 }
 
@@ -13,34 +13,195 @@ InputClass::~InputClass()
 {
 }
 
-void InputClass::Initialize()
+bool InputClass::Initialize(HINSTANCE hinstance, HWND hwnd, int screenWidth, int screenHeight)
 {
-	int i;
+	// 마우스 커서의 위치 지정에 사용될 화면 크기를 설정한다.
+	m_screenWidth = screenWidth;
+	m_screenHeight = screenHeight;
 
-	// 모든 키들을 눌리지 않은 상태로 초기화 한다.
-	for(i = 0; i < 256; i++)
+	// Direct Input 인터페이스를 초기화 합니다.
+	HRESULT result = DirectInput8Create(hinstance, DIRECTINPUT_VERSION, IID_IDirectInput8, reinterpret_cast<void**>(&m_directInput), nullptr);
+	if (FAILED(result))
 	{
-		m_keys[i] = false;
+		return false;
 	}
 
-	return;
+	// 키보드의 Direct Input 인터페이스를 생성한다.
+	result = m_directInput->CreateDevice(GUID_SysKeyboard, &m_keyboard, nullptr);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// 데이터 형식을 설정한다. 이 경우 키보드이므로 사전 정의된 데이터 형식을 사용 할 수 있다.
+	result = m_keyboard->SetDataFormat(&c_dfDIKeyboard);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// 키보드를 할당 받는다.
+	result = m_keyboard->Acquire();
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// 마우스 Direct Input 인터페이스를 생성한다.
+	result = m_directInput->CreateDevice(GUID_SysMouse, &m_mouse, nullptr);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// 미리 정의된 마우스 데이터 형식을 사용하여 마우스의 데이터 형식을 설정한다.
+	result = m_mouse->SetDataFormat(&c_dfDIMouse);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// 다른 프로그램과 공유 할 수 있도록 마우스의 협력 수준을 설정한다.
+	result = m_mouse->SetCooperativeLevel(hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE);
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	// 마우스를 할당받는다.
+	result = m_mouse->Acquire();
+	if (FAILED(result))
+	{
+		return false;
+	}
+
+	return true;
 }
 
-
-bool InputClass::IsKeyDown(unsigned int key)
+void InputClass::Shutdown()
 {
-	// 현재키 상태 반환
-	return m_keys[key];
+	// 마우스 반환
+	if (m_mouse)
+	{
+		m_mouse->Unacquire();
+		m_mouse->Release();
+		m_mouse = nullptr;
+	}
+
+	// 키보드 반환
+	if (m_keyboard)
+	{
+		m_keyboard->Unacquire();
+		m_keyboard->Release();
+		m_keyboard = nullptr;
+	}
+
+	// m_directInput 객체 반환
+	if (m_directInput)
+	{
+		m_directInput->Release();
+		m_directInput = nullptr;
+	}
 }
 
-void InputClass::KeyDown(unsigned int input)
+bool InputClass::Frame()
 {
-	// 키가 눌렸다면 그 상태를 배열에 저장
-	m_keys[input] = true;
+	// 키보드의 현재 상태를 읽는다.
+	if (!ReadKeyboard())
+	{
+		return false;
+	}
+
+	// 마우스의 현재 상태를 읽는다.
+	if (!ReadMouse())
+	{
+		return false;
+	}
+
+	// 키보드와 마우스의 변경상태를 처리한다.
+	ProcessInput();
+
+	return true;
 }
 
-void InputClass::KeyUp(unsigned int input)
+bool InputClass::IsEscapePressed()
 {
-	// 키가 떼어졌다면 그 상태를 배열에 저장
-	m_keys[input] = false;
+	// escape 키가 현재 눌려지고 있는지 bit값을 계산하여 확인한다.
+	if (m_keyboardState[DIK_ESCAPE] & 0x80)
+	{
+		return true;
+	}
+	return false;
+}
+
+void InputClass::GetMouseLocation(int& mouseX, int& mouseY)
+{
+	mouseX = m_mouseX;
+	mouseY = m_mouseY;
+}
+
+bool InputClass::ReadKeyboard()
+{
+	// 키보드 디바이스를 얻는다.
+	HRESULT result = m_keyboard->GetDeviceState(sizeof(m_keyboardState), reinterpret_cast<LPVOID>(&m_keyboardState));
+	if (FAILED(result))
+	{
+		// 키보드가 포커스를 잃었거나 획득되지 않은 경우 컨트롤을 다시 가져온다.
+		if ((result == DIERR_INPUTLOST) || (result == DIERR_NOTACQUIRED))
+		{
+			m_keyboard->Acquire();
+		}
+		else
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+bool InputClass::ReadMouse()
+{
+	// 마우스 디바이스를 얻는다.
+	HRESULT result = m_mouse->GetDeviceState(sizeof(DIMOUSESTATE), reinterpret_cast<LPVOID>(&m_mouseState));
+	if (FAILED(result))
+	{
+		// 마우스가 포커스를 잃었거나 획득되지 않은 경우 컨트롤을 다시 가져온다.
+		if ((result == DIERR_INPUTLOST) || (result == DIERR_NOTACQUIRED))
+		{
+			m_mouse->Acquire();
+		}
+		else
+		{
+			return false;
+		}
+	}
+	return true;
+}
+
+void InputClass::ProcessInput()
+{
+	// 프레임 동안 마우스 위치의 변경을 기반으로 마우스 커서의 위치를 업데이트 한다.
+	m_mouseX += m_mouseState.lX;
+	m_mouseY += m_mouseState.lY;
+
+	// 마우스 위치가 화면 너비 또는 높이를 초과하지 않는지 확인한다.
+	if (m_mouseX < 0)
+	{
+		m_mouseX = 0;
+	}
+	if (m_mouseY < 0)
+	{
+		m_mouseY = 0;
+	}
+
+	if (m_mouseX > m_screenWidth)
+	{
+		m_mouseX = m_screenWidth;
+	}
+	if (m_mouseY > m_screenHeight)	
+	{
+		m_mouseY = m_screenHeight;
+	}
+
+	
 }
--- Tutorial/inputclass.h
+++ Tutorial/inputclass.h
@@ -6,16 +6,33 @@ class InputClass
 {
 public:
 	InputClass();
-	InputClass(const InputClass&);
+	InputClass(const InputClass& other);
 	~InputClass();
 
-	void Initialize();
-	bool IsKeyDown(unsigned int);
-	void KeyDown(unsigned int);
-	void KeyUp(unsigned int);
+	bool Initialize(HINSTANCE hinstance, HWND hwnd, int screenWidth, int screenHeight);
+	void Shutdown();
+	bool Frame();
+
+	bool IsEscapePressed();
+	void GetMouseLocation(int& mouseX, int& mouseY);
 
 private:
-	bool m_keys[256];
+	bool ReadKeyboard();
+	bool ReadMouse();
+	void ProcessInput();
+
+private:
+	IDirectInput8* m_directInput = nullptr;
+	IDirectInputDevice8* m_keyboard = nullptr;
+	IDirectInputDevice8* m_mouse = nullptr;
+
+	unsigned char m_keyboardState[256] = { 0, };
+	DIMOUSESTATE m_mouseState;
+
+	int m_screenWidth = 0;
+	int m_screenHeight = 0;
+	int m_mouseX = 0;
+	int m_mouseY = 0;
 };
 
 #endif // _INPUTCLASS_H_
