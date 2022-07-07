0744459d7c11353c1a9587b086c03fa3d7e37178
woonhak-kong <gamdekong@gmail.com>
2022-07-07 16:12:49 -0400

    DxDefine is modified for adding dinamic linking file for DirectX Input

--- Tutorial/DxDefine.h
+++ Tutorial/DxDefine.h
@@ -1,15 +1,17 @@
 ﻿#pragma once
-#pragma once
 
 // 라이브러리 링크
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "dinput8.lib")
+
 
 
 // include
 #include <d3d11.h>
 #include <d3dcompiler.h>
 #include <directxmath.h>
+#include <dinput.h>
 using namespace DirectX;
 
